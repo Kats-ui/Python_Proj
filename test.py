c84d7d71f9475f91e0eeae270909307a9665c1d4
Kats-ui <franzjay.5379@gmail.com>
2026-01-17 20:20:04 +0800

    Simple Change
    num3 to total

--- test.py
+++ test.py
@@ -1,21 +1,21 @@
 # Mini classic calculator
 num1 = int(input("Enter a number: "))
 num2 = int(input("Enter a number: "))
-num3 = 0
+total = 0
 operation = input("Enter an operation (+, -, *, /): ")
 
 if operation == "+":
-    num3 = num1 + num2
-    print("The result is:", num3)
+    total = num1 + num2
+    print("The result is:", total)
 elif operation == "-":
-    num3 = num1 - num2
-    print("The result is:", num3)
+    total = num1 - num2
+    print("The result is:", total)
 elif operation == "*":
-    num3 = num1 * num2
-    print("The result is:", num3)
+    total = num1 * num2
+    print("The result is:", total)
 elif operation == "/":
-    num3 = num1 / num2
-    print("The result is:", num3)
+    total = num1 / num2
+    print("The result is:", total)
 else:
     print("Invalid operation")
 
